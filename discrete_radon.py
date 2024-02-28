9055bbe1c637f927fe0cd31800d42993713fdb6c
София <sofiatrufanowa@yandex.ru>
2024-02-28 22:03:54 +0300

    added DRT and 5 results of it

--- discrete_radon.py
+++ discrete_radon.py
@@ -17,39 +17,38 @@ def radon_transform(img):
         Тут используется - Nearest neighbour interpolation, которая выражает n:
         n = round((p_k * x_m + tau_h - y_min) / delta_y)
     """
+    img = np.ones(img.shape) * 255 - img
+    N = img.shape[1]  # y
+    M = img.shape[0]  # x
+    H, K = 301, 301
+    delta_x, delta_y = 2 / (M - 1), 2 / (N - 1)
+    x_min, y_min = 0, 0
+    p_min, tau_min = np.pi, 10
+    p = np.linspace(-p_min, p_min, K)
+    tau = np.linspace(-tau_min, tau_min, H)
 
-    p_min = -50  # пробегаюсь по прямым с коэффициентами от -50 до 50, чтобы было с запасом :)
-    delta_p = 0.02
-    K = 5001
-
-    def p(k):
-        return p_min + k * delta_p
-
-    tau_min = 0
-    delta_tau = 1
-    H = 512  # тоже с запасом беру прямые
-
-    def tau(h):
-        return tau_min + h * delta_tau
-
-    x_min = y_min = 0
-    M = img.shape[1]
-    N = img.shape[0]
-    delta_x = delta_y = 1
-
-    img_radon[k, h] = []
+    img_radon = np.zeros((K, H))
 
     for k in range(K):
         for h in range(H):
-            alpha = p(k) * delta_x / delta_y
-            beta = (p(k) * x_min + tau(h0) - y_min) / Delta_y
+            alpha = p[k] * delta_x / delta_y
+            beta = (p[k] * x_min + tau[h] - y_min) / delta_y
+            m_min = 0
+            m_max = M - 1
+            if alpha > 0:
+                m_min = int(max(0, np.ceil((- beta + 0.5) / alpha)))
+                m_max = int(min(M - 1, np.floor((N - 0.5 - beta) / alpha)))
+            if alpha < 0:
+                m_min = int(max(0, np.ceil((N - 0.5 - beta) / alpha)))
+                m_max = int(min(M - 1, np.floor((- beta + 0.5) / alpha)))
             summa = 0
-            for m in range(M):
+            for m in range(m_min, m_max + 1):
                 n = round(alpha * m + beta)
-                if (n >= 0) & (n <= N):
+                if (n >= 0) & (n < N):
                     summa += img[m, n]
-            img_radon[k, h] = Delta_x * summa
+            img_radon[k, h] = delta_x * summa
     return img_radon
+
 
 # считаем картинки
 
