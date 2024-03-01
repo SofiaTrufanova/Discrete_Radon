fixed one small bug - there were wrong sign

discrete_radon.py
@@ -20,9 +20,9 @@ def radon_transform(img):
     img = np.ones(img.shape) * 255 - img
     N = img.shape[1]  # y
     M = img.shape[0]  # x
-    H, K = 301, 301
+    H, K = 501, 501
     delta_x, delta_y = 2 / (M - 1), 2 / (N - 1)
-    x_min, y_min = 0, 0
+    x_min, y_min = -1, -1
     p_min, tau_min = np.pi, 10
     p = np.linspace(-p_min, p_min, K)
     tau = np.linspace(-tau_min, tau_min, H)
@@ -36,11 +36,11 @@ def radon_transform(img):
             m_min = 0
             m_max = M - 1
             if alpha > 0:
-                m_min = int(max(0, np.ceil((- beta + 0.5) / alpha)))
+                m_min = int(max(0, np.ceil((- beta - 0.5) / alpha)))
                 m_max = int(min(M - 1, np.floor((N - 0.5 - beta) / alpha)))
             if alpha < 0:
                 m_min = int(max(0, np.ceil((N - 0.5 - beta) / alpha)))
-                m_max = int(min(M - 1, np.floor((- beta + 0.5) / alpha)))
+                m_max = int(min(M - 1, np.floor((- beta - 0.5) / alpha)))
             summa = 0
             for m in range(m_min, m_max + 1):
                 n = round(alpha * m + beta)